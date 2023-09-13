2fe706df2ed2a8a519393b2d93e1d0c61261feba
AlyssaBlair <alyssascholl@gmail.com>
2023-09-13 15:57:34 -0700

    First commit of last word

---
+++ last_word.py
@@ -0,0 +1,11 @@
+
+def length_of_last_word (s):
+
+	splitString = s.split()
+	letterCount = 0
+	lengthOfSplitString = len(splitString)
+	lastWord = splitString[lengthOfSplitString - 1]
+	return len(lastWord)
+
+s = "luffy is still joyboy"
+print(length_of_last_word(s))
