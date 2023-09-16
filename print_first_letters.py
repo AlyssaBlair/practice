dfe9da48891f8ac3615e076a3808fac5d36f09ef
AlyssaBlair <alyssascholl@gmail.com>
2023-09-16 16:47:01 -0700

    First commit

---
+++ print_first_letters.py
@@ -0,0 +1,12 @@
+
+#Write a function to print the first letter of every word in a string.
+
+def print_first_letters(s):
+
+	split_string = s.split()
+
+	for i in range(0, len(split_string)):
+		print(split_string[i][0])
+
+s = "This is a test of this code"
+print_first_letters(s)
