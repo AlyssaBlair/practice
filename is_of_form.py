5d6b9a4bce6be5d8e069f6457917dd3be90caee6
AlyssaBlair <alyssascholl@gmail.com>
2023-09-16 18:19:49 -0700

    First commit

---
+++ is_of_form.py
@@ -0,0 +1,20 @@
+
+#Check to see if a number is in the form (2^n) - 1
+
+def is_of_form(x):
+
+	x = x - 1
+
+	if x % 2 != 0:
+		return False
+
+	else:
+		while x != 1:
+			x = x / 2
+			if x == 1:
+				return True
+			if x % 2 != 0:
+				return False
+
+x = 16
+print(is_of_form(x))
