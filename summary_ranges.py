7678ab12d3e70d6c0c894e4fec3d78e6cf3bff34
AlyssaBlair <alyssascholl@gmail.com>
2023-09-17 17:15:35 -0700

    Fixed it so the case of the first and second being different won't result in leaving out the first element

--- summary_ranges.py
+++ summary_ranges.py
@@ -1,28 +1,26 @@
 
+#
+
 def summary_ranges(nums):
 
-	range_array = []
+	current_range = [nums[0]]
 	final_array = []
-	
-	for i in range(0, len(nums) - 1):	
-		if nums[i] != nums[i + 1] - 1:
-			range_array = []
-			range_array.append(nums[i + 1])
-			final_array.append(range_array)
-			print(range_array)
 
-		elif nums[i] == nums[i + 1] - 1:
-			if nums[i] not in range_array:
-				final_array.append(range_array)
-				range_array.append(nums[i])
-			range_array.append(nums[i + 1])
-			print(range_array)
-	for i in range(0, len(final_array)):
-		if len(final_array[i]) == 1:
-			print(final_array[i], "has one number")
+	for i in range(1, len(nums)):
+		if nums[i] - nums[i - 1] == 1:
+			current_range.append(nums[i])
 		else:
-			final_array[i] = [final_array[i][0], "->", final_array[i][len(final_array[i]) - 1]]
-	print(final_array)
+			final_array.append(current_range)
+			current_range = [nums[i]]
+
+	final_array.append(current_range)
+
+	for i in range(0, len(final_array)):
+
+		if len(final_array[i]) == 1:
+			print(final_array[i][0])
+		else:
+			print(final_array[i][0], "->", final_array[i][len(final_array[i]) - 1])
 
 nums = [0, 3, 4, 5, 6, 8, 9]
 summary_ranges(nums)
