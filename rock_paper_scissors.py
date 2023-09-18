0f82c4743630db529bf9318b5062b8b990c69789
AlyssaBlair <alyssascholl@gmail.com>
2023-09-18 09:15:05 -0700

    First commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,31 @@
+
+#Create a basic rock, paper, scissors program
+
+def rock_paper_scissors(user_choice):
+
+	user_choice = user_choice.lower()
+
+	wins_map = {
+		"rock": "scissors",
+		"scissors": "paper",
+		"paper": "rock"
+	}
+
+	import random
+	computer_choice = random.randint(0, 2)
+	choices = ["rock", "paper", "scissors"]
+	computer_choice = choices[computer_choice]
+
+	print("Computer choice is:", computer_choice)
+
+	if wins_map[computer_choice] == user_choice:
+		return "Winner: computer!"
+
+	if wins_map[user_choice] == computer_choice:
+		return "Winner: you!"
+
+	if user_choice == computer_choice:
+		return "It's a draw!"
+
+user_choice = input("Rock, paper, scissors, shoot! (Type in your choice of weapon) \n" )
+print(rock_paper_scissors(user_choice))
