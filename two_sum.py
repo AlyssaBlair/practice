68656a4ddcb6ed72971b48f564582743dd4b199e
AlyssaBlair <alyssascholl@gmail.com>
2023-09-17 17:52:44 -0700

    Got rid of an unnecessary break statement

--- two_sum.py
+++ two_sum.py
@@ -15,8 +15,7 @@ def two_sum(nums, target):
 		if target - nums[i] in sumsMap.keys() and nums[i] != target / 2:
 			for j in range(0, len(nums)):
 				if nums[j] == target - nums[i]:
-					break			
-			return i, j
+					return i, j			
 
 nums = [2, 11, 15, 7]
 target = 9
