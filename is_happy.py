1490df66e692713c5415f57a65ebaa1ae5e53f11
AlyssaBlair <alyssascholl@gmail.com>
2024-02-07 10:25:29 -0800

    Added description, removed print statements used to debug

--- is_happy.py
+++ is_happy.py
@@ -1,25 +1,36 @@
 
+# Checks to see if a number is "happy." A number is called happy if it leads 
+# to 1 after a sequence of steps wherein each step number is replaced by the 
+# sum of the squares of its digits. That is, if we start with a "happy" number 
+# and keep replacing it with the sum of its digits squares, we reach 1. For 
+# example, 19 is "happy", because 1^2 + 9^2 = 82, 8^2 + 2^2 = 68, 6^2 + 8^2 = 100, 
+# and 1^2 + 0^2 + 0^2 = 1. As we reached to 1, 19 is a "happy" number.
+
 def is_happy(n, map_new_numbers):
 
 	n_as_string = str(n)
 	new_number = 0
 	
 	for i in range(0, len(n_as_string)):
+		
 		new_number += int(n_as_string[i]) * int(n_as_string[i])
+
 		if i == len(n_as_string) - 1:
-			#print("new number is", new_number)
+			
 			if new_number in map_new_numbers:
-				#print("new number in map already, False")
 				return False
+
 			if new_number not in map_new_numbers:
 				map_new_numbers[new_number] = 1
-				#print(map_new_numbers)
+				
 			if new_number == 1:
-				#print("we made it here, new number is 1, True")
 				return True
+
 			result = is_happy(new_number, map_new_numbers)
+
 			if result:
 				return True
+
 	return False
 			
 n = 19
