f061054968048c6bf1b9c9d726f18be98f6000d9
AlyssaBlair <alyssascholl@gmail.com>
2024-02-07 11:07:15 -0800

    Cleaned up and clarified comment on what the code does and re tested

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,10 @@
 
-#assuming each input has exactly one solution, cannot use same element twice, returning the index of the two numbers that create the sum
+# Assuming each input has exactly one solution, and cannot use 
+# the same element twice, return the index of the two numbers 
+# in the array that create the target sum. This code does not 
+# account for a situation in which the target cannot be reached 
+# by adding exactly two of the elements of the array together. 
+# This assumes there is exactly one solution.
 
 def two_sum(nums, target):
 
@@ -9,7 +14,7 @@ def two_sum(nums, target):
 		if nums[i] <= target:
 			sumsMap[nums[i]] = target - nums[i]
 	
-	#search nums for the target minus the number
+	# search nums for the target minus the number
 
 	for i in range(0, target):
 		if target - nums[i] in sumsMap.keys() and nums[i] != target / 2:
@@ -18,5 +23,5 @@ def two_sum(nums, target):
 					return i, j			
 
 nums = [2, 11, 15, 7]
-target = 9
+target = 22
 print(two_sum(nums, target))
