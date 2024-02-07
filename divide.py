61d078b4b8cd8f04c22429394ba66419eeb7afcd
AlyssaBlair <alyssascholl@gmail.com>
2024-02-07 08:41:48 -0800

    Added functionality for dividing with negative numbers

--- divide.py
+++ divide.py
@@ -1,26 +1,38 @@
 
-# Implement a divide function without using /, %, etc.
+# Implement a divide function without using /, %, *, etc.
 
-# I can't figure out how to account for negative numbers in an efficient 
-# way without doing a bunch of if statements. I'm sure it has to do with 
-# the abs() function but I can't figure out which values need to be the 
-# absolute value, I'll come back to this with fresh eyes later. Also the
-# fraction part of answers that aren't whole numbers are not simplified,
+# The fraction part of answers that aren't whole numbers are not simplified,
 # but that's just gonna have to do for now lol
 
 def divide(x, y):
 
+	solution = ""
+	
+	# If only one of the divisor or dividend is negative, make the
+	# solution negative
+
+	if (x < 0 and y > 0) or (x > 0 and y < 0):
+		solution += "-"
+
+	x = abs(x)
+	y = abs(y)
+
 	if y == 0:
 		return "Undefined"
 
+	if x == 0:
+		return 0
+
 	if y == x:
-		return 1
+		solution += str(1)
+		return solution
 
 	if x < y:
 		fraction = str(x)
 		fraction += "/"
 		fraction += str(y)
-		return fraction
+		solution += fraction
+		return solution
 
 	original_y = y
 
@@ -29,13 +41,14 @@ def divide(x, y):
 		y += original_y
 		
 		if y == x:
-			return i
+			solution += str(i)
+			return solution
 		
 		if y > x:
 			modulo = abs(x - (y - original_y))
-			solution = str(i - 1) + " " + divide(modulo, original_y)
-			return solution 
+			solution += str(i - 1) + " " + divide(modulo, original_y)
+			return solution
 
-x = 20
-y = 6
+x = 90
+y = 88
 print(divide(x, y))
