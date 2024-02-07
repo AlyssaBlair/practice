2463a09e00d89364c8dadd91bf0d65c2f20dc661
AlyssaBlair <alyssascholl@gmail.com>
2024-02-06 18:34:54 -0800

    Added spaces after #s to make comments more readable

--- consecutive_sums.py
+++ consecutive_sums.py
@@ -1,12 +1,13 @@
 
-#Given an array of positive integers and a target integer, find if there 
-#is a consecutive subarray that sums to the target. 
-#E.g, given {5,6,4,12}, findsum(10)=true, findsum(18)=false.
+# Given an array of positive integers and a target integer, find if there 
+# is a consecutive subarray that sums to the target. 
+# E.g, given {5,6,4,12}, findsum(10)=true, findsum(18)=false.
 
-#I'm realizing now that this code only checks if there is a consecutive 
-#subarray of size two that sums to the target. So setting the target to 
-#14 with the array [5, 6, 4, 12] returns False, when it should return 
-#True becuase 5 + 6 + 4 = 14. I'll have to keep working on this one.
+
+# I'm realizing now that this code only checks if there is a consecutive 
+# subarray of size two that sums to the target. So setting the target to 
+# 14 with the array [5, 6, 4, 12] returns False, when it should return 
+# True becuase 5 + 6 + 4 = 14. I'll have to keep working on this one.
 
 def consecutive_sum(nums):
 
